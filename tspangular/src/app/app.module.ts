e8c61457da6b75a65d438a433f358b4aa1119b21
mostofa-rezvi <mostofa.aminur.rezvi@gmail.com>
2024-07-09 16:15:27 +0600

    TSP

--- tspangular/src/app/app.module.ts
+++ tspangular/src/app/app.module.ts
@@ -5,19 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentComponent } from './student/student.component';
 import { EmployeeComponent } from './employee/employee.component';
+import { LocationComponent } from './location/location.component';
+import { CreatelocationComponent } from './createlocation/createlocation.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     StudentComponent,
-    EmployeeComponent
+    EmployeeComponent,
+    LocationComponent,
+    CreatelocationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
-    provideClientHydration()
+    // provideClientHydration()
+    provideHttpClient(
+      withFetch()
+    )
   ],
   bootstrap: [AppComponent]
 })
