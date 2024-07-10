0766080779c6f28bea8f797622fff86af9b0308b
mostofa-rezvi <mostofa.aminur.rezvi@gmail.com>
2024-07-10 19:03:11 +0600

    TSP

--- tspangular/src/app/app-routing.module.ts
+++ tspangular/src/app/app-routing.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { StudentComponent } from './student/student.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { LocationComponent } from './location/location.component';
 import { CreatelocationComponent } from './createlocation/createlocation.component';
 import { UpdatelocationComponent } from './updatelocation/updatelocation.component';
+import { ViewstudentComponent } from './student/viewstudent/viewstudent.component';
 
 const routes: Routes = [
-  {path: 'student', component: StudentComponent},
+
   {path: 'employee', component: EmployeeComponent},
   {path: 'location', component: LocationComponent},
   {path: 'createlocation', component: CreatelocationComponent},
   {path: 'updatelocation/:id', component: UpdatelocationComponent},
+  {path: 'students', component: ViewstudentComponent},
 ];
 
 @NgModule({
---
+++ tspangular/src/app/student/student.model.ts
@@ -0,0 +1,9 @@
+import { Location } from "../location/location.model";
+
+export class StudentModel{
+    id !: number;
+    name !: string;
+    email !: string;
+    cell !: string;
+    location !: string;
+}
