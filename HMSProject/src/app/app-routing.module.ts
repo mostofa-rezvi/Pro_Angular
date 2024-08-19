106d073a5c0dc9f2bda1298a984f92c761a8cb72
mostofa-rezvi <mostofa.aminur.rezvi@gmail.com>
2024-08-20 04:19:46 +0600

    Add

--- HMSProject/src/app/app-routing.module.ts
+++ HMSProject/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppointmentComponent } from './homepage/appointment/appointment.component';
+import { BodypartComponent } from './homepage/bodypart/bodypart.component';
 
 const routes: Routes = [
-  {path: 'appointment', component: AppointmentComponent}
+  {path: 'appointment', component: AppointmentComponent},
+  {path: '' , component: BodypartComponent},
+  { path: '', redirectTo: '/appointment', pathMatch: 'full' }
 ];
 
 @NgModule({
