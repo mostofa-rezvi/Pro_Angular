c1d8479768d1460b54f544491e83cbe21ef5eadd
mostofa-rezvi <mostofa.aminur.rezvi@gmail.com>
2024-08-21 16:03:05 +0600

    Add

--- HMSProject/src/app/app-routing.module.ts
+++ HMSProject/src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppointmentComponent } from './homepage/appointment/appointment.component';
 import { BodypartComponent } from './homepage/bodypart/bodypart.component';
+import { LoginComponent } from './registration/login/login.component';
+import { RegisterComponent } from './registration/register/register.component';
 
 const routes: Routes = [
-  {path: 'appointment', component: AppointmentComponent},
   {path: '' , component: BodypartComponent},
-  { path: '', redirectTo: '/appointment', pathMatch: 'full' }
+  {path: 'appointment', component: AppointmentComponent},
+  {path: '', redirectTo: '/appointment', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: '**', redirectTo: '/login'},
+  {path: 'register', component: RegisterComponent}
 ];
 
 @NgModule({
--- HMSProject/src/app/app.module.ts
+++ HMSProject/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { NavbarComponent } from './homepage/navbar/navbar.component';
 import { FooterComponent } from './homepage/footer/footer.component';
 import { BodypartComponent } from './homepage/bodypart/bodypart.component';
 import { AppointmentComponent } from './homepage/appointment/appointment.component';
+import { LoginComponent } from './registration/login/login.component';
+import { RegisterComponent } from './registration/register/register.component';
+import { ForgotpasswordComponent } from './registration/forgotpassword/forgotpassword.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { AppointmentComponent } from './homepage/appointment/appointment.compone
     NavbarComponent,
     FooterComponent,
     BodypartComponent,
-    AppointmentComponent
+    AppointmentComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgotpasswordComponent
   ],
   imports: [
     BrowserModule,
