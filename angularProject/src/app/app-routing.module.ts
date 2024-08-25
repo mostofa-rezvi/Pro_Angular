955e17059ebda262b0a4f192327e16b94de01afb
mostofa-rezvi <mostofa.aminur.rezvi@gmail.com>
2024-08-25 14:05:43 +0600

    v-9 14m

--- angularProject/src/app/app-routing.module.ts
+++ angularProject/src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { EmployeeComponent } from './employee/employee.component';
 import { ActivitiesComponent } from './dashboard/activities/activities.component';
 
 const routes: Routes = [
-  {path: 'employee', component: EmployeeComponent},
   {path: 'activities', component: ActivitiesComponent}
 ];
 
