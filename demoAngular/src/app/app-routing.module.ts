9a21a71725fa7a9c053f2cfbf3b9e0de12f793a5
mostofa-rezvi <mostofa.aminur.rezvi@gmail.com>
2024-08-27 01:42:50 +0600

    student

--- demoAngular/src/app/app-routing.module.ts
+++ demoAngular/src/app/app-routing.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SidebarComponent } from './Board/sidebar/sidebar.component';
-import { LocationComponent } from './DemoWork/location/location.component';
-import { LocationcreateComponent } from './DemoWork/locationcreate/locationcreate.component';
-import { LocationupdateComponent } from './DemoWork/locationupdate/locationupdate.component';
+import { LocationComponent } from './Location/location/location.component';
+import { LocationcreateComponent } from './Location/locationcreate/locationcreate.component';
+import { LocationupdateComponent } from './Location/locationupdate/locationupdate.component';
+import { StudentviewComponent } from './Student/studentview/studentview.component';
+import { StudentaddComponent } from './Student/studentadd/studentadd.component';
+import { StudentupdateComponent } from './Student/studentupdate/studentupdate.component';
 
 
 const routes: Routes = [
   {path: 'sidebar', component: SidebarComponent},
   {path: 'location', component: LocationComponent},
   {path: 'locationcreate', component: LocationcreateComponent},
-  {path: 'locationupdate/:id', component: LocationupdateComponent}
+  {path: 'locationupdate/:id', component: LocationupdateComponent},
+  {path: 'studentview', component: StudentviewComponent},
+  {path: 'studentadd', component: StudentaddComponent},
+  {path: 'studentupdate/:id', component: StudentupdateComponent}
 ];
 
 @NgModule({
--- demoAngular/src/app/app.module.ts
+++ demoAngular/src/app/app.module.ts
@@ -7,11 +7,14 @@ import { TeacherComponent } from './DemoWork/teacher/teacher.component';
 import { NavbarComponent } from './Home/navbar/navbar.component';
 import { FooterComponent } from './Home/footer/footer.component';
 import { SidebarComponent } from './Board/sidebar/sidebar.component';
-import { LocationComponent } from './DemoWork/location/location.component';
-import { LocationcreateComponent } from './DemoWork/locationcreate/locationcreate.component';
-import { LocationupdateComponent } from './DemoWork/locationupdate/locationupdate.component';
+import { LocationComponent } from './Location/location/location.component';
+import { LocationcreateComponent } from './Location/locationcreate/locationcreate.component';
+import { LocationupdateComponent } from './Location/locationupdate/locationupdate.component';
 import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { StudentviewComponent } from './Student/studentview/studentview.component';
+import { StudentupdateComponent } from './Student/studentupdate/studentupdate.component';
+import { StudentaddComponent } from './Student/studentadd/studentadd.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +25,10 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
     SidebarComponent,
     LocationComponent,
     LocationcreateComponent,
-    LocationupdateComponent
+    LocationupdateComponent,
+    StudentviewComponent,
+    StudentupdateComponent,
+    StudentaddComponent
   ],
   imports: [
     BrowserModule,
